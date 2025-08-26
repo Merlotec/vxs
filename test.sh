e472cb1a706b9c83fba6eb4363feefd01424e120
Brodie Knight <ncbmknight@gmail.com>
2025-08-26 12:04:56 +0300

    Delete test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-cd voxelsim-py
-maturin build --release --features px4 
-cd ../python
-pip install /Users/brodieknight/dev/drone/vxs/voxelsim-py/target/wheels/voxelsim_py-0.1.0-cp313-cp313-macosx_11_0_arm64.whl --force-reinstall
-python3 povtest.py
