efbfda97b6442e613a02de87690b0a33f100e600
Brodie Knight <brodieknight@MBP-3.local>
2025-10-22 20:06:04 +0100

    color change

--- voxelsim-renderer/src/pov.rs
+++ voxelsim-renderer/src/pov.rs
@@ -113,7 +113,10 @@ pub fn begin_render(
                 .set(RenderPlugin {
                     render_creation: bevy::render::settings::RenderCreation::Automatic(
                         bevy::render::settings::WgpuSettings {
-                            backends: Some(bevy::render::settings::Backends::BROWSER_WEBGPU),
+                            backends: Some(
+                                bevy::render::settings::Backends::BROWSER_WEBGPU
+                                    | bevy::render::settings::Backends::PRIMARY,
+                            ),
                             ..Default::default()
                         },
                     ),
